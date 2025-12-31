3ec6c53a37523b943bc4685ac7d26e0b4186ee4c
Ali Raza Attari <attaraliraza4@gmail.com>
2025-12-31 17:10:18 +0500

    Last Koshishi

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,24 +1,12 @@
-// // config/cors.config.ts
-// import cors from 'cors';
-// import { FRONTEND_URL, isProduction } from './app.config';
-
-// export const corsOptions: cors.CorsOptions = {
-//   origin: [
-//     FRONTEND_URL,
-//     ...(isProduction ? [] : ['http://localhost:3000']),
-//   ],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-//   credentials: true,
-//   optionsSuccessStatus: 200, // legacy browser support
-// };
-
-
-
+// config/cors.config.ts
 import cors from 'cors';
+import { FRONTEND_URL, isProduction } from './app.config';
 
 export const corsOptions: cors.CorsOptions = {
-  origin: ['http://localhost:3000'], // exact frontend origin
+  origin: [
+    FRONTEND_URL,
+    ...(isProduction ? [] : ['http://localhost:3000']),
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   credentials: true,
