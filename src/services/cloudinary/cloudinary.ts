2009a6a61721ef14b3dc128a60bca6a867b12a88
Ali Raza Attari <attaraliraza4@gmail.com>
2025-12-31 22:52:53 +0500

    fffffff

--- src/services/cloudinary/cloudinary.ts
+++ src/services/cloudinary/cloudinary.ts
@@ -1,14 +1,26 @@
-// services/cloudinary.ts
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
 
-export const uploadOnCloudinary = async (
-  buffer: Buffer
-): Promise<string | null> => {
-  return new Promise((resolve) => {
+dotenv.config();
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECERET_KEY,
+});
+
+// Function to upload file to Cloudinary
+
+export const uploadOnCloudinary = async (buffer: Buffer) => {
+  return new Promise<string | null>((resolve) => {
     cloudinary.uploader.upload_stream(
       { resource_type: "auto" },
       (error, result) => {
-        if (error) return resolve(null);
+        if (error) {
+          console.error("Cloudinary Error:", error);
+          return resolve(null);
+        }
         resolve(result?.secure_url || null);
       }
     ).end(buffer);
